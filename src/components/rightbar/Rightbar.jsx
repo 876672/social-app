ac2f42bb370da9304ab179047066f98d7e502275
Shubham Sakhare <shubham@reldyn.co>
2024-02-01 01:33:52 +0530

    first commit and added the design part

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,162 @@
+import './rightbar.css';
+export default function Rightbar() {
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pala foster</b> and <b> 3 other friends </b> have birthday today
+          </span>
+        </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>{' '}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">john carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
